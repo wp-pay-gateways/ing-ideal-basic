edf32bf367762e32ecb5a342167e9d9c93831482
Remco Tolsma <info@remcotolsma.nl>
2015-11-11 15:01:04 +0100

    Added integration class.

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # WordPress Pay Gateway: ING - iDEAL Basic
 
 **ING - iDEAL Basic driver for the WordPress payment processing library.**
+
+## Production Environment
+
+**Dashboard URL:** https://ideal.secure-ing.com/  
+**Payment Server URL:** https://ideal.secure-ing.com/ideal/mpiPayInitIng.do  
+
+## Test Environment
+
+**Dashboard URL:** https://idealtest.secure-ing.com/  
+**Payment Server URL:** https://idealtest.secure-ing.com/ideal/mpiPayInitIng.do  
---
+++ src/Integration.php
@@ -0,0 +1,21 @@
+<?php
+
+class Pronamic_WP_Pay_Gateways_ING_IDealBasic_IDealBasic_Integration {
+	public function __construct() {
+		$this->id       = 'ing-ideal-basic';
+		$this->name     = 'ING - iDEAL Basic';
+		$this->provider = 'ing';
+	}
+
+	public function get_config_factory_class() {
+		return 'Pronamic_WP_Pay_Gateways_IDealBasic_ConfigFactory';
+	}
+
+	public function get_config_class() {
+		return 'Pronamic_WP_Pay_Gateways_IDealBasic_Config';
+	}
+
+	public function get_gateway_class() {
+		return 'Pronamic_WP_Pay_Gateways_IDealBasic_Gateway';
+	}
+}
