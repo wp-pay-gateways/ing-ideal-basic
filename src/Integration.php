43f1862c355b3b22eb994f2f832d38179e988ade
Remco Tolsma <info@remcotolsma.nl>
2015-11-19 13:35:13 +0100

    Extend from iDEAL Basic library.

--- src/Integration.php
+++ src/Integration.php
@@ -1,21 +1,9 @@
 <?php
 
-class Pronamic_WP_Pay_Gateways_ING_IDealBasic_Integration {
+class Pronamic_WP_Pay_Gateways_ING_IDealBasic_Integration extends Pronamic_WP_Pay_Gateways_IDealBasic_AbstractIntegration {
 	public function __construct() {
 		$this->id       = 'ing-ideal-basic';
 		$this->name     = 'ING - iDEAL Basic';
 		$this->provider = 'ing';
-	}
-
-	public function get_config_factory_class() {
-		return 'Pronamic_WP_Pay_Gateways_IDealBasic_ConfigFactory';
-	}
-
-	public function get_config_class() {
-		return 'Pronamic_WP_Pay_Gateways_IDealBasic_Config';
-	}
-
-	public function get_gateway_class() {
-		return 'Pronamic_WP_Pay_Gateways_IDealBasic_Gateway';
 	}
 }
