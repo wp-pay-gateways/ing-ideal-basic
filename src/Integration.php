f2c27349eab05ad08d41ace31f8c25bee810446a
Remco Tolsma <info@remcotolsma.nl>
2015-11-20 15:59:22 +0100

    Call parent construct.

--- src/Integration.php
+++ src/Integration.php
@@ -10,6 +10,8 @@
  */
 class Pronamic_WP_Pay_Gateways_ING_IDealBasic_Integration extends Pronamic_WP_Pay_Gateways_IDealBasic_AbstractIntegration {
 	public function __construct() {
+		parent::__construct();
+
 		$this->id       = 'ing-ideal-basic';
 		$this->name     = 'ING - iDEAL Basic';
 		$this->provider = 'ing';
