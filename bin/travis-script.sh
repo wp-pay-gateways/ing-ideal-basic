aa05ff5b90749f1a63ae6a46911cd30426bb91bb
Remco Tolsma <info@remcotolsma.nl>
2015-12-02 14:09:48 +0100

    Added support for Grunt.

--- bin/travis-script.sh
+++ bin/travis-script.sh
@@ -2,9 +2,19 @@
 
 set -ev
 
+# PHPUnit
 phpunit
 
+# Coveralls
 if [[ '1' == $COVERAGE ]]; then
 	php ../vendor/bin/coveralls
 fi
 
+# Grunt
+if [[ '1' == $GRUNT ]]; then
+	npm install -g grunt-cli
+
+	npm install --no-optional
+
+	grunt
+fi
